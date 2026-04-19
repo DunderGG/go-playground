9dae2cdf99c30e22cd4c709c96d7089f9607747d
David Bennehag <david.bennehag@gmail.com>
2026-04-19 16:32:05 +0200

    Move GoPurge to its own repository

--- AssetCleanup/discovery/discovery_unix.go
+++
@@ -1,8 +0,0 @@
-//go:build !windows
-
-package discovery
-
-// applyMaxPathPrefix is a no-op on non-Windows platforms.
-func applyMaxPathPrefix(path string) string {
-	return path
-}
--- AssetCleanup/go.mod
+++
@@ -1,3 +0,0 @@
-module GoPurge
-
-go 1.26.1
